031202a6df9c27131d1267673276910c2e52398b
Biel Sala <biel.sala@autonoma.cat>
2024-05-26 19:40:18 +0200

    Rewriting of subcommand "parsing"

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 extern crate home;
 use std::fs;
 use std::path::PathBuf;
-use std::env;
 
 const PROGRAM_NAME: &str = "pr";
+const PROGRAM_VERSION: &str = "0.1.0";
 
-fn get_config_path() -> PathBuf {
+fn get_passwords_path() -> PathBuf {
     match home::home_dir() {
         Some(mut home_dir) => {
             home_dir.push(".config");
             home_dir.push(PROGRAM_NAME);
+            home_dir.push("passwords"); // ~/.config/pr/passwords
             home_dir
         }
         None => {
-            eprintln!("Why the fuck do you not have a HOME env variable");
+            eprintln!("[ERROR] 'HOME' environment variable not set");
             std::process::exit(1);
         }
     }
@@ -21,65 +22,58 @@ fn get_config_path() -> PathBuf {
 
 fn check_config_dir(path: PathBuf) {
     if !path.exists() {
-        eprintln!("INFO: Creating missing config path: {}", path.as_path().display().to_string());
+        eprintln!("[INFO] Creating missing passwords directory: {}", path.as_path().display().to_string());
         match fs::create_dir_all(path.clone()) {
-            Ok(()) => eprintln!("INFO: Config directory created"),
-            Err(e) => eprintln!("Error: Could not create config directory: {e:?}"),
+            Ok(()) => eprintln!("[INFO] Passwords directory created"),
+            Err(e) => eprintln!("[ERROR] Could not create config directory: {e:?}"),
         }
-        assert!(path.exists(), "Unreachable: config path somehow disappeared?");
+        assert!(path.exists(), "[UNREACHABLE] config path somehow disappeared");
     }
-
 }
 
-fn cmd_help() {
+fn cmd_help() -> Result<(), ()> {
     println!("Usage: {} [command] [...]", PROGRAM_NAME);
-    println!("    Commands:");
-    println!("        help\tPrint this message");
-    println!("        list\tList password directory");
-    println!();
-
-    std::process::exit(0);
+    println!("Commands:");
+    println!("\thelp Print this message");
+    println!("\tlist List passwords");
+    Ok(())
 }
 
-fn cmd_list() {
-    let path = get_config_path();
-    println!("Contents of {}:", path.as_path().display().to_string());
-    // TODO
-    std::process::exit(0);
+fn cmd_version() -> Result<(), ()> {
+    println!("{} version: {}", PROGRAM_NAME, PROGRAM_VERSION);
+    Ok(())
+}
+
+fn cmd_list() -> Result<(), std::io::Error> {
+    let passwords_path = get_passwords_path();
+    println!("Contents of {}:", passwords_path.as_path().display().to_string());
+    check_config_dir(passwords_path.clone());
+    for entry in fs::read_dir(passwords_path)? {
+        let entry = entry?;
+        println!("\t{:?}", entry.file_name());
+    }
+    Ok(())
+}
+
+fn cmd_new() -> Result<(), ()> { // TODO: Read password, master password, encrypt and save to file
 }
 
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        cmd_help();
-    }
+    let subcmd = std::env::args().nth(1).expect("[ERROR] No subcommand given");
 
-    let ref subcommand = args.remove(1);
-
-    let config_path = get_config_path();
-
-    check_config_dir(config_path);
-
-
-    match subcommand.as_str() {
+    match subcmd.as_str() {
         "help" => {
-            if args.len() > 1 {
-                println!("Too many args");
-                std::process::exit(1);
-            }
-            cmd_help();
+            cmd_help().expect("[ERROR] Unexpected error while executing subcommand 'help'")
+        }
+        "version" => {
+            cmd_version().expect("[ERROR] Unexpected error while executing subcommand 'version'")
         }
         "list" => {
-            if args.len() > 1 {
-                println!("Too many args");
-                std::process::exit(1);
-            }
-            cmd_list();
+            cmd_list().expect("[ERROR] Unexpected error while executing subcommand 'list'")
         }
         _ => {
-            println!("Unknown subcommand '{}'", subcommand);
+            eprintln!("[ERROR] Unknown subcommand '{}'", subcmd);
             std::process::exit(1);
         }
     }
-
 }
